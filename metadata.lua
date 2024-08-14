441f1b03342b04021e43188994ea53f37222b3ba
waldenp0nd <20667094+waldenp0nd@users.noreply.github.com>
2024-08-14 12:09:04 +0800

    fix: GetAddOnMetadata update

--- metadata.lua
+++ metadata.lua
@@ -1,9 +1,9 @@
 local ADDON_NAME, ns = ...
 
 ns.name = "Tol Barad When?"
-ns.title = GetAddOnMetadata(ADDON_NAME, "Title")
-ns.notes = GetAddOnMetadata(ADDON_NAME, "Notes")
-ns.version = GetAddOnMetadata(ADDON_NAME, "Version")
-ns.icon = GetAddOnMetadata(ADDON_NAME, "IconAtlas")
+ns.title = C_AddOns.GetAddOnMetadata(ADDON_NAME, "Title")
+ns.notes = C_AddOns.GetAddOnMetadata(ADDON_NAME, "Notes")
+ns.version = C_AddOns.GetAddOnMetadata(ADDON_NAME, "Version")
+ns.icon = C_AddOns.GetAddOnMetadata(ADDON_NAME, "IconAtlas")
 ns.color = "c1e0ba"
 ns.command = "tbw"
